aa5a3d2588231678d3032d3a31c6e391ce9635ac
sky <sky.stebnicki@aereus.com>
2015-11-15 08:26:46 -0800

    Stubbed out the recurrence component for entity forms

--- client/web/js/ui/entity/form/Recurrence.jsx
+++ client/web/js/ui/entity/form/Recurrence.jsx
@@ -12,9 +12,33 @@ var Recurrence = React.createClass({
     render: function() {
 
         var xmlNode = this.props.xmlNode;
-        return (
-            <div>Recurrence</div>
-        );
+        var fieldName = xmlNode.getAttribute('name');
+        var field = this.props.entity.def.getField(fieldName);
+        var fieldValue = this.props.entity.getValue(fieldName);
+
+        // TODO: We have to load the recurrence plugin here and handle updating this.props.entity
+
+        if (this.props.editMode) {
+
+            return (
+                <div>Put recurrence plugin here</div>
+            );
+
+
+        } else {
+
+            // If there is no value then we don't need to show this field at all
+            if (!fieldValue) {
+                return (<div />);
+            } else {
+                return (
+                    <div>
+                        <div className="entity-form-field-label">Repeats</div>
+                        <div className="entity-form-field-value">Human Description Here</div>
+                    </div>
+                );
+            }
+        }
     }
 
 });
