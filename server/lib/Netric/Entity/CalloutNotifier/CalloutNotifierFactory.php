94bd44286d8d0d8ab020261459b7b90d982ecae6
sky <sky.stebnicki@aereus.com>
2015-12-24 12:33:54 -0800

    Started working on the design for the callout notifier

---
+++ server/lib/Netric/Entity/CalloutNotifier/CalloutNotifierFactory.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author Sky Stebnicki <sky.stebnicki@aereus.com>
+ * @copyright 2015 Aereus
+ */
+namespace Netric\Entity;
+
+/**
+ * Service factory for the CalloutNotifier
+ */
+class CalloutNotifier
+{
+}
