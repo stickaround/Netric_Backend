f7dd2972dc462775e2fa217a51bec7ae6a9d4e8e
sky <sky.stebnicki@aereus.com>
2016-01-05 08:45:28 -0800

    Fixed some problems with the object reference field not filtering correctly

--- client/web/js/ui/entity/form/Objectsref.jsx
+++ client/web/js/ui/entity/form/Objectsref.jsx
@@ -10,6 +10,18 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var CustomEventTrigger = require("../../mixins/CustomEventTrigger.jsx");
 var controller = require("../../../controller/controller");
+var Device = require("../../../Device");
+var Where = require("../../../entity/Where");
+
+/**
+ * Constant indicating the smallest device that we can print a browser in
+ *
+ * All other devices will open browsers in a dialog when clicked
+ *
+ * @type {number}
+ * @private
+ */
+var _minimumInlineDeviceSize = Device.sizes.large;
 
 /**
  * Objectsref/entityList element
@@ -18,34 +30,28 @@ var Objectsref = React.createClass({
 
     mixins: [CustomEventTrigger],
 
+    getInitialState: function () {
+        return {
+            entityController: null,
+            entityId: this.props.entity.id
+        }
+    },
+
     /**
      * Render the browser after the component mounts
      */
     componentDidMount: function() {
-        // Require EntityBrowserController here so we do not risk a circular dependency
-        var EntityBrowserController = require("../../../controller/EntityBrowserController");
+        this._loadEntities();
+    },
 
-        var xmlNode = this.props.xmlNode;
-        var name = xmlNode.getAttribute('name');
-        var objType = xmlNode.getAttribute('obj_type');
-        var refField = xmlNode.getAttribute('ref_field');
-
-        var data = {
-            type: controller.types.FRAGMENT,
-            hideToolbar: true,
-            objType: objType,
-            eventsObj: this.props.eventsObj,
-            onEntityClick: function(objType, oid) {
-                this.sendEntityClickEvent_(objType, oid);
-            }.bind(this)
-        }
-
-        // Add filter for to reference current entity
-        data[refField] = this.props.entity.getValue(refField);
-
-        // Create browser and render
-        var browser = new EntityBrowserController();
-        browser.load(data, ReactDOM.findDOMNode(this.refs.bcon));
+    /**
+     * Invoked immediately after the component's updates are flushed to the DOM.
+     *
+     * This method is not called for the initial render and we use it to check if
+     * the id has changed - meaning a new entity was saved.
+     */
+    componentDidUpdate: function () {
+        this._loadEntities();
     },
 
     /**
@@ -64,6 +70,62 @@ var Objectsref = React.createClass({
      */
     sendEntityClickEvent_: function(objType, oid) {
         this.triggerCustomEvent("entityclick", {objType:objType, id:oid});
+    },
+
+    /**
+     * Load the entity browser controller either inline or as dialog for smaller devices
+     *
+     * @private
+     */
+    _loadEntities: function () {
+
+        // Only load comments if this device displays inline comments (size > medium)
+        if (netric.getApplication().device.size < _minimumInlineDeviceSize) {
+            return;
+        }
+
+        // We only display comments if workign with an actual entity
+        if (!this.props.entity.id) {
+            return;
+        }
+
+        // Check if we have already loaded the controller
+        if (this.state.entityController) {
+            // Just refresh the results and return
+            this.state.entityController.refresh();
+            return;
+        }
+
+        // Require EntityBrowserController here so we do not risk a circular dependency
+        var EntityBrowserController = require("../../../controller/EntityBrowserController");
+
+        var xmlNode = this.props.xmlNode;
+        var name = xmlNode.getAttribute('name');
+        var objType = xmlNode.getAttribute('obj_type');
+        var refField = xmlNode.getAttribute('ref_field');
+
+        var whereCond = new Where(refField);
+        whereCond.equalTo(this.props.entity.id);
+
+        var data = {
+            type: controller.types.FRAGMENT,
+            hideToolbar: true,
+            objType: objType,
+            filters: [ whereCond ],
+            eventsObj: this.props.eventsObj,
+            onEntityClick: function(objType, oid) {
+                this.sendEntityClickEvent_(objType, oid);
+            }.bind(this)
+        }
+
+        // Add filter for to reference current entity
+        data[refField] = this.props.entity.getValue(refField);
+
+        // Create browser and render
+        var browser = new EntityBrowserController();
+        browser.load(data, ReactDOM.findDOMNode(this.refs.bcon));
+
+        this.setState({entityController: browser});
     }
 
 });
