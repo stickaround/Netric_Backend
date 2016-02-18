68355222087f3cfc56018667fd95e283550fdf61
marl.tumulak <marl.tumulak@aereus.com>
2016-02-18 23:39:20 +0800

    Updated the browser_view for opportunity. reverted back the removed conditions

--- server/objects/browser_views/opportunity.php
+++ server/objects/browser_views/opportunity.php
@@ -7,23 +7,34 @@ namespace objects\browser_views;
 use Netric\EntityQuery\Where;
 
 return array(
-    'my_open_opportunities'=> array(
+	'my_open_opportunities'=> array(
 		'obj_type' => 'opportunity',
 		'name' => 'My Open Opportunities',
 		'description' => 'Opportunities assigned to me that are not closed',
 		'default' => true,
 		'conditions' => array(
-
-        ),
+			'owner' => array(
+				'blogic' => Where::COMBINED_BY_AND,
+				'field_name' => 'owner_id',
+				'operator' => Where::OPERATOR_EQUAL_TO,
+				'value' => -3
+			),
+			'closed' => array(
+				'blogic' => Where::COMBINED_BY_AND,
+				'field_name' => 'f_closed',
+				'operator' => Where::OPERATOR_NOT_EQUAL_TO,
+				'value' => 't'
+			),
+		),
 		'order_by' => array(
 			'date' => array(
-    			'field_name' => 'ts_entered',
-    			'direction' => 'desc',
-    		),
+				'field_name' => 'ts_entered',
+				'direction' => 'desc',
+			),
 		),
 		'table_columns' => array('name', 'customer_id', 'stage_id', 'amount', 'expected_close_date', 'probability_per', 'type_id')
-    ),
-		
+	),
+
 	'all_my_opportunities'=> array(
 		'obj_type' => 'opportunity',
 		'name' => 'All My Opportunities',
@@ -38,14 +49,14 @@ return array(
 			),
 		),
 		'order_by' => array(
-				'date' => array(
-					'field_name' => 'ts_entered',
-					'direction' => 'desc',
-				),
+			'date' => array(
+				'field_name' => 'ts_entered',
+				'direction' => 'desc',
+			),
 		),
 		'table_columns' => array('name', 'customer_id', 'stage_id', 'amount', 'expected_close_date', 'probability_per', 'owner_id')
 	),
-		
+
 	'all_open_opportunities'=> array(
 		'obj_type' => 'opportunity',
 		'name' => 'All Open Opportunities',
@@ -67,7 +78,7 @@ return array(
 		),
 		'table_columns' => array('name', 'customer_id', 'stage_id', 'amount', 'expected_close_date', 'probability_per', 'owner_id')
 	),
-	
+
 	'all_opportunities'=> array(
 		'obj_type' => 'opportunity',
 		'name' => 'All Opportunities',
