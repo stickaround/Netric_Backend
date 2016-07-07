7ff61e260bed391ee0b1bd4dd8c4dad6e0488118
sky <sky.stebnicki@aereus.com>
2016-07-07 07:55:57 -0700

    Making sure that webpack config is updated

--- client/web/webpack.config.js
+++ client/web/webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-
 module.exports = {
     context: __dirname,
     entry: "./js/main.js",
