9d48e76e8dd93210a2fb07d23e70e4ad88d0462e
sky <sky.stebnicki@aereus.com>
2016-08-10 06:30:45 -0700

    Updated tests to run with 256M for reports

--- docker/server/bin/netric-tests.sh
+++ docker/server/bin/netric-tests.sh
@@ -5,4 +5,4 @@ set -e
 export PHP_IDE_CONFIG="serverName=devel.netric.com"
 
 cd /var/www/html/tests
-../vendor/bin/phpunit -c phpunit.xml $@
+../vendor/bin/phpunit -c phpunit.xml -d memory_limit=256M $@
