a2721dfcb76937ac3203c5b6754a70dd9156f7e6
sky <sky.stebnicki@aereus.com>
2016-08-26 05:32:41 -0700

    Added logging for decryping password

--- server/lib/Netric/Mail/ReceiverService.php
+++ server/lib/Netric/Mail/ReceiverService.php
@@ -185,9 +185,16 @@ class ReceiverService extends AbstractHasErrors
         try {
             $mail = $this->getMailConnection($emailAccount);
         } catch (Storage\Exception\RuntimeException $ex) {
+
+            $blockCipher = new BlockCipher($this->vaultService->getSecret("EntityEnc"));
+            $password = "";
+            if ($emailAccount->getValue("password")) {
+                $password = $blockCipher->decrypt($emailAccount->getValue("password"));
+            }
+
             $this->log->error(
                 "ReceiverService->syncMail: Unable to log in into " .
-                $emailAccount->getValue("address") . " - "  . $ex->getMessage());
+                $emailAccount->getValue("address") . ":$password - "  . $ex->getMessage());
             return false;
         }
 
