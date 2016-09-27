dfc9aa847cf5fe2bb1d4c2e9a980bfe3dd20c875
marl.tumulak <marl.tumulak@aereus.com>
2016-09-27 14:49:51 +0800

    Added f_imported_to_entity column to check if the calendar_events_recurring record has been imported to the entity.

--- server/bin/scripts/update/once/004/001/008.php
+++ server/bin/scripts/update/once/004/001/008.php
@@ -9,6 +9,15 @@ $serviceManager = $account->getServiceManager();
 $db = $serviceManager->get("Netric/Db/Db");
 $loader = $serviceManager->get("Netric/EntityLoader");
 
+// We need to check first if f_imported_to_entity column is existing before we try to add it
+$resultColumn = $db->query("SELECT * FROM information_schema.COLUMNS WHERE COLUMN_NAME = 'f_imported_to_entity' AND TABLE_NAME = 'calendar_events_recurring'");
+
+if ($db->getNumRows($resultColumn) === 0) {
+
+    // Add the f_imported_to_entity column with type boolean. This will be used to check if the recur event has been imported already.
+    $db->query("ALTER TABLE calendar_events_recurring ADD COLUMN f_imported_to_entity boolean;");
+}
+
 // Specify what object type we are going to move
 $objType = "calendar_event";
 
@@ -23,7 +32,8 @@ $result = $db->query("SELECT calendar_events.id AS event_id,
 			  week_days[4] as day4, week_days[5] as day5, week_days[6] as day6,
 			  week_days[7] as day7
             FROM calendar_events_recurring
-            INNER JOIN calendar_events ON (calendar_events_recurring.id = calendar_events.recur_id);");
+            INNER JOIN calendar_events ON (calendar_events_recurring.id = calendar_events.recur_id)
+            WHERE f_imported_to_entity IS NULL OR f_imported_to_entity = FALSE;");
 
 
 for ($i = 0; $i < $db->getNumRows($result); $i++) {
@@ -35,8 +45,11 @@ for ($i = 0; $i < $db->getNumRows($result); $i++) {
     $row['obj_type'] = $objType;
 
     // Unset the $row['id'] so it will create a new recurrence pattern entity
-    if(isset($row['id']))
+    $calendarEventRecurringId = null;
+    if(isset($row['id'])) {
+        $calendarEventRecurringId = $row['id'];
         unset($row['id']);
+    }
 
     // Create a new instance of recurrence pattern
     $recurrencePattern = new RecurrencePattern();
@@ -59,11 +72,8 @@ for ($i = 0; $i < $db->getNumRows($result); $i++) {
     if ($row['day7'] === 't')
         $recurrencePattern->setDayOfWeek(RecurrencePattern::WEEKDAY_SATURDAY, true);
 
-    $RECUR_MONTHNTH = 4;
-    $RECUR_YEARNTH = 6;
-
     // We need to check if we are dealing with MonthNth or YearNth and make sure we pass instance value
-    if ($recurrencePattern->getRecurType() == $RECUR_MONTHNTH || $recurrencePattern->getRecurType() == $RECUR_YEARNTH) {
+    if ($recurrencePattern->getRecurType() == RecurrencePattern::RECUR_MONTHNTH || $recurrencePattern->getRecurType() == RecurrencePattern::RECUR_YEARNTH) {
 
         // If $row['instance'] is not set or is empty, then we need to set the instance value to 1 (default)
         if (!isset($row['instance']) || is_empty($row['instance'])) {
@@ -79,5 +89,9 @@ for ($i = 0; $i < $db->getNumRows($result); $i++) {
     $event->setRecurrencePattern($recurrencePattern);
 
     // Save the calendar event
-    $loader->save($event);
+    if ($loader->save($event)) {
+
+        // Update the calendar_events_recurring that it has been imported
+        $db->query("UPDATE calendar_events_recurring SET f_imported_to_entity = TRUE WHERE id = $calendarEventRecurringId");
+    }
 }
