ff01f85f979908b64a2f0790ff630eec0b046500
Sky Stebnicki <sky.stebnicki@aereus.com>
2017-06-05 14:44:13 +0300

    Fixed permissions on every test run

--- docker/server/bin/netric-tests.sh
+++ docker/server/bin/netric-tests.sh
@@ -6,3 +6,10 @@ export PHP_IDE_CONFIG="serverName=devel.netric.com"
 
 cd /var/www/html/tests
 ../vendor/bin/phpunit -c phpunit.xml -d memory_limit=256M $@
+
+# Make sure permissions are set in generated data files
+chmod -R 777 ./tmp
+chmod -R 777 ../data/tmp
+chmod -R 777 ../data/log
+chmod -R 777 ../data/profile_runs
+chmod -R 777 ../data/z-push
