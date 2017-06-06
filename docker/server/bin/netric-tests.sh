ec5f9b086aaf819975f09ecd7ade1b1586056e68
Sky Stebnicki <sky.stebnicki@aereus.com>
2017-06-06 12:37:06 +0300

    Added data/tmp to chmod in tests

--- docker/server/bin/netric-tests.sh
+++ docker/server/bin/netric-tests.sh
@@ -8,4 +8,5 @@ cd /var/www/html/tests
 ../vendor/bin/phpunit -c phpunit.xml -d memory_limit=256M $@
 
 # Make sure permissions are set in generated data files
-chmod -R 777 ./tmp
+chmod -R 777 ./tmp/
+chmod -R 777 ./data/
