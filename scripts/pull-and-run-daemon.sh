df24fa91f384f8e124421de0bd7141dd54329321
Sky Stebnicki <sky.stebnicki@aereus.com>
2017-06-18 09:37:15 -0700

    Fixed startup script

--- scripts/pull-and-run-daemon.sh
+++ scripts/pull-and-run-daemon.sh
@@ -14,6 +14,6 @@ docker pull docker.aereusdev.com:5001/netric:${TARGET}
 # Run the daemon with bin/netricd start-fg (start foreground)
 docker stop netricd
 docker rm netricd
-docker run -P -d --restart=unless-stopped --name netricd \
-    -e APPLICATION_ENV="production" --log-driver=syslog --log-opt tag=netric-latest \
-    --log-opt syslog-facility=local2 docker.aereusdev.com:5001/netric:latest /start-daemon.sh
+docker run -P -d --restart=on-failure--name netricd \
+    -e APPLICATION_ENV="production" --log-driver=syslog --log-opt tag=netric-${TARGET} \
+    --log-opt syslog-facility=local2 docker.aereusdev.com:5001/netric:${TARGET} /start-daemon.sh
