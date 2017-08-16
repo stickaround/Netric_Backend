9487ca509628b2f28dc970a4ee59d756bc1d3694
Sky Stebnicki <sky.stebnicki@aereus.com>
2017-08-16 08:53:05 -0700

    Fixed the order of chown in setup because it was trying to change folders that existed in a parent dir

--- docker/server/bin/netric-setup.sh
+++ docker/server/bin/netric-setup.sh
@@ -5,11 +5,11 @@ cd /var/www/html/
 php composer.phar install
 php composer.phar update
 
-cd /var/www/html/bin
-
 # Make sure we can write to required files
 chown -R www-data:www-data data/log
 chown -R www-data:www-data data/tmp
+
+cd /var/www/html/bin
 
 # Run install in case this is our first run - it will exit gracefully if
 # netric was previously installed
