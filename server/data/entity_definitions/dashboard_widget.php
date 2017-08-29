984c3a9efd4c03aaec310802d97975a69d0aa7d8
marl.tumulak <marl.tumulak@aereus.com>
2017-08-29 23:04:15 +0800

    Added the new dashboard_widget entity
    Summary: Add the new dashboard_widget entity
    
    Test Plan: dashboard_widget entity should be added
    
    Reviewers: sky
    
    Differential Revision: http://src.aereusdev.com/D287

---
+++ server/data/entity_definitions/dashboard_widget.php
@@ -0,0 +1,47 @@
+<?php
+namespace data\entity_definitions;
+
+return array(
+    'revision' => 1,
+    'fields' => array(
+        'dashboard_id' => array(
+            'title' => 'Dashboard',
+            'type' => 'object',
+            'subtype' => 'dashboard'
+        ),
+        'widget_id' => array(
+            'title'=>'Widget Id',
+            'type'=>'number',
+            'subtype'=>'',
+            'required'=>true,
+            'readonly'=>false
+        ),
+        'widget' => array(
+            'title'=>'Widget',
+            'type'=>'text',
+            'subtype'=>'256',
+            'readonly'=>true
+        ),
+        'col' => array(
+            'title'=>'Column',
+            'type'=>'number',
+            'subtype'=>'',
+            'required'=>true,
+            'readonly'=>false
+        ),
+        'pos' => array(
+            'title'=>'Position',
+            'type'=>'number',
+            'subtype'=>'',
+            'required'=>true,
+            'readonly'=>false
+        ),
+        'data' => array(
+            'title'=>'Data',
+            'type'=>'text',
+            'subtype'=>'32',
+            'readonly'=>true,
+            'default'=>array("value"=>"", "on"=>"null")
+        ),
+    ),
+);
