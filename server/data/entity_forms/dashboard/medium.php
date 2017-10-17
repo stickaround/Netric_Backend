59ede1bd3a77c3597262ee68776336e2a4903cb6
marl.tumulak <marl.tumulak@aereus.com>
2017-10-17 22:46:41 +0800

    Updated the entity forms for dashboard.
    Summary: Entity dashboard should display properly using the entity forms for mobile devices
    
    Test Plan: Open the netric client in mobile view and it should display properly
    
    Reviewers: sky
    
    Differential Revision: https://src.aereusdev.com/D312

--- server/data/entity_forms/dashboard/medium.php
+++ server/data/entity_forms/dashboard/medium.php
@@ -1,33 +1,31 @@
-<fieldset name='Details' editmodeonly='t'>
-    <row>
-        <column>
-            <field name='name'></field>
-        </column>
-    </row>
-    <row>
-        <column>
-            <field name='description' multiline='t'></field>
-        </column>
-    </row>
-    <row>
-        <column>
-            <field name='owner_id'></field>
-        </column>
-    </row>
-    <row>
-        <column>
-            <field name='scope'
-                   tooltip="If set to User then only the Owner has access to this dashboard. It is private. Otherwise, if set to System/Everyone then everyone who has permission to view dashboards can access it."></field>
-        </column>
-    </row>
-    <row>
-        <column>
-            <field name='num_columns'></field>
-        </column>
-    </row>
-    <row>
-        <column>
-            <plugin name='DashboardWidgets'></plugin>
-        </column>
-    </row>
-</fieldset>
+<column>
+    <fieldset name='Details' editmodeonly='t'>
+        <row>
+            <column>
+                <field name='name'></field>
+            </column>
+        </row>
+        <row>
+            <column>
+                <field name='description' multiline='t'></field>
+            </column>
+        </row>
+        <row>
+            <column>
+                <field name='owner_id'></field>
+            </column>
+        </row>
+        <row>
+            <column>
+                <field name='scope'
+                       tooltip="If set to User then only the Owner has access to this dashboard. It is private. Otherwise, if set to System/Everyone then everyone who has permission to view dashboards can access it."></field>
+            </column>
+        </row>
+        <row>
+            <column>
+                <field name='num_columns'></field>
+            </column>
+        </row>
+    </fieldset>
+    <plugin name='DashboardWidgets'></plugin>
+</column>
