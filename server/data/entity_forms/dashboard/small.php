698291428b0c5f921f613134db3079cf9ad24a6c
marl.tumulak <marl.tumulak@aereus.com>
2017-10-18 20:23:15 +0800

    Added row before the column on dashboard entity forms

--- server/data/entity_forms/dashboard/small.php
+++ server/data/entity_forms/dashboard/small.php
@@ -1,11 +1,13 @@
-<column>
-    <fieldset name='Details' editmodeonly='t'>
-        <field name='name'></field>
-        <field name='description' multiline='t'></field>
-        <field name='owner_id'></field>
-        <field name='scope'
-               tooltip="If set to User then only the Owner has access to this dashboard. It is private. Otherwise, if set to System/Everyone then everyone who has permission to view dashboards can access it."></field>
-        <field name='num_columns'></field>
-    </fieldset>
-    <plugin name='DashboardWidgets'></plugin>
-</column>
+<row>
+    <column>
+        <fieldset name='Details' editmodeonly='t'>
+            <field name='name'></field>
+            <field name='description' multiline='t'></field>
+            <field name='owner_id'></field>
+            <field name='scope'
+                   tooltip="If set to User then only the Owner has access to this dashboard. It is private. Otherwise, if set to System/Everyone then everyone who has permission to view dashboards can access it."></field>
+            <field name='num_columns'></field>
+        </fieldset>
+        <plugin name='DashboardWidgets'></plugin>
+    </column>
+</row>
