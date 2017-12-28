512162a180bd99b9a5237cec4b1cd2e79e547f0e
Sky Stebnicki <sky@stebnicki.net>
2017-12-28 13:48:21 -0800

    Fixing the start script to pause to let mogile settle

--- docker/server/bin/start.sh
+++ docker/server/bin/start.sh
@@ -21,7 +21,8 @@ chown -R www-data /var/www/html/data/
 
 # If run setup was passed as an environment variable then execute before starting the server
 if [ "${RUN_SETUP}" ]; then
-	echo "Running netric setup"
+	echo "Running netric setup after resting for 10 seconds"
+	sleep 10
 	/netric-setup.sh
 fi
 
