97f47d363faa6f164c8982fa094a4858573efdd0
marl.tumulak <marl.tumulak@aereus.com>
2017-12-29 08:13:42 +0800

    Implemented the entity forms for CMS Site. Also fixed the entity definitions for CMS site and content feed.
    Summary: Content CMS Site and Feed should be working properly
    
    Test Plan: Open netric client, CMS Site should be working
    
    Reviewers: sky
    
    Differential Revision: https://src.aereusdev.com/D360

--- server/data/entity_definitions/cms_site.php
+++ server/data/entity_definitions/cms_site.php
@@ -2,7 +2,7 @@
 namespace data\entity_definitions;
 
 return array(
-    'revision' => 8,
+    'revision' => 9,
     'fields' => array(
         // Textual name
         'name' => array(
@@ -37,6 +37,16 @@ return array(
             'autocreate'=>true, // Create foreign object automatically
             'autocreatebase'=>'/System/Objects/cms_site', // Where to create (for folders, the path with no trail slash)
             'autocreatename'=>'id', // the field to pull the new object name from
+        ),
+
+        "owner_id" => array(
+            'title'=>'Owner',
+            'type'=>'object',
+            'subtype'=>'user',
+            'default'=>array(
+                "value"=>"-3",
+                "on"=>"null"
+            ),
         ),
     ),
 );
---
+++ server/data/entity_forms/cms_site/large.php
@@ -0,0 +1,43 @@
+<row>
+    <column>
+        <field name='name'></field>
+    </column>
+</row>
+<row>
+    <column>
+        <row>
+            <column width='50%'>
+                <field name='url'></field>
+                <field name='url_test'></field>
+            </column>
+            <column width='50%'>
+                <field name='owner_id'></field>
+                <field name='ts_entered'></field>
+            </column>
+        </row>
+        <row>
+            <column>
+                <tabs>
+                    <tab name='Feeds &amp; Blogs'>
+                        <objectsref obj_type='content_feed' ref_field='site_id' />
+                    </tab>
+                    <tab name='Pages'>
+                        <objectsref obj_type='cms_page' ref_field='site_id'></objectsref>
+                    </tab>
+                    <tab name='Page Templates'>
+                        <objectsref obj_type='cms_page_template' ref_field='site_id'></objectsref>
+                    </tab>
+                    <tab name='Snippets'>
+                        <objectsref obj_type='cms_snippet' ref_field='site_id'></objectsref>
+                    </tab>
+                    <tab name='Comments'>
+                        <field name='comments'></field>
+                    </tab>
+                    <tab name='Media'>
+                        <field hidelabel='t' name='folder_id'></field>
+                    </tab>
+                </tabs>
+            </column>
+        </row>
+    </column>
+</row>
