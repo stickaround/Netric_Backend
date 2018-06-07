75862619a91c070edd588f76fb14c86a53cff764
Sky Stebnicki <sky.stebnicki@aereus.com>
2018-06-06 20:32:10 -0700

    Fixed deploy. Now trying to figure out how to manage logging

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,7 +16,10 @@ fi
 
 docker login -u aereusdev -p p7pfsGRe dockerhub.aereusdev.com
 
-docker pull dockerhub.aereusdev.com/netric:${APPLICATION_VER}
-
 # Update the docker stack
+export APPLICATION_ENV=$APPLICATION_ENV
+export APPLICATION_VER=$APPLICATION_VER
+echo "APPLICATION_ENV ${APPLICATION_ENV}"
+echo "APPLICATION_VER ${APPLICATION_VER}"
+
 docker stack deploy -c docker-compose-stack.yml --with-registry-auth netric_com
