894a4fe9e58802bf03f063ce421b836959e316c3
Sky Stebnicki <sky.stebnicki@aereus.com>
2018-06-09 15:25:52 -0700

    Fixed issue with creating overlapping data column

--- data/schema/account.php
+++ data/schema/account.php
@@ -1208,7 +1208,7 @@ return array(
             'tsv_fulltext' => array('type' => SchemaProperty::TYPE_TEXT_TOKENS),
             'num_comments' => array('type' => SchemaProperty::TYPE_INT),
             'commit_id' => array('type' => SchemaProperty::TYPE_BIGINT),
-            'data' => array('type' => SchemaProperty::TYPE_JSON),
+            'field_data' => array('type' => SchemaProperty::TYPE_JSON),
         )
     ),
 
