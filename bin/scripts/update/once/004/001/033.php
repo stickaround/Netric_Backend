14c602026a350a43e74699bec1302cde0991fded
marl.tumulak <marl.tumulak@gmail.com>
2020-03-23 23:34:24 +0800

    Change the update once 033.php to just update the objects_task table.

--- bin/scripts/update/once/004/001/033.php
+++ bin/scripts/update/once/004/001/033.php
@@ -1,10 +1,11 @@
 <?php
 /**
- * Get all entities in objects table, and update each entity's object reference.
+ * Get all tasks in objects_task table, and update each task's object reference.
  */
 use Netric\Db\Relational\RelationalDbFactory;
 use Netric\Entity\DataMapper\DataMapperFactory;
 use Netric\Entity\EntityFactoryFactory;
+use Netric\EntityDefinition\ObjectTypes;
 
 $account = $this->getAccount();
 $serviceManager = $account->getServiceManager();
@@ -18,25 +19,23 @@ $db->beginTransaction();
 // Do not timeout for this long query
 $db->query('set statement_timeout to 0');
 
-$result = $db->query("SELECT guid, id, field_data FROM objects");
+$result = $db->query("SELECT * FROM objects_task");
 
 // Commit the transaction
 $db->commitTransaction();
 
-
 foreach ($result->fetchAll() as $rowData) {
     $entityData = json_decode($rowData['field_data'], true);
     
-    if (!$entityData || empty($entityData['obj_type'])) {
-        continue;
-    }
-
     if (empty($entityData['id'])) {
         $entityData['id'] = $rowData['id'];
     }
 
-    $entityData['guid'] = $rowData['guid'];
-    $entity = $entityFactory->create($entityData['obj_type']);
+    if (empty($entityData['guid'])) {
+        $entityData['guid'] = $rowData['guid'];
+    }
+
+    $entity = $entityFactory->create(ObjectTypes::TASK);
     $entity->fromArray($entityData);
     $entityDm->updatObjectReferencesToGuid($entity);
 }
