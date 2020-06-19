d94e939e508d89991d8cfd1368ae441de13b2a1a
Sky Stebnicki <sky.stebnicki@aereus.com>
2020-06-18 17:43:17 -0700

    Removed array

--- data/browser_views/task.php
+++ data/browser_views/task.php
@@ -10,192 +10,192 @@ use Netric\Entity\ObjType\UserEntity;
 use Netric\Entity\ObjType\TaskEntity;
 use Netric\EntityQuery\Where;
 
-return array(
-    'my_tasks' => array(
+return [
+    'my_tasks' => [
         'obj_type' => 'task',
         'name' => 'My Incomplete Tasks',
         'description' => 'Incomplete tasks assigned to me',
         'default' => true,
-        'conditions' => array(
-            'user' => array(
+        'conditions' => [
+            'user' => [
                 'blogic' => Where::COMBINED_BY_AND,
                 'field_name' => 'owner_id',
                 'operator' => Where::OPERATOR_EQUAL_TO,
                 'value' => UserEntity::USER_CURRENT,
-            ),
-            'status_id' => array(
+            ],
+            'status_id' => [
                 'blogic' => Where::COMBINED_BY_AND,
                 'field_name' => 'status_id',
                 'operator' => Where::OPERATOR_NOT_EQUAL_TO,
                 'value' => TaskEntity::STATUS_COMPLETED
-            ),
-        ),
+            ],
+        ],
         'group_first_order_by' => true,
-        'order_by' => array(
-            'status_id' => array(
+        'order_by' => [
+            'status_id' => [
                 'field_name' => 'status_id',
                 'direction' => 'desc',
-            ),
-            'date' => array(
+            ],
+            'date' => [
                 'field_name' => 'ts_entered',
                 'direction' => 'desc',
-            ),
-            'deadline' => array(
+            ],
+            'deadline' => [
                 'field_name' => 'deadline',
                 'direction' => 'asc'
-            ),
-        ),
-        'table_columns' => array('name', 'project', 'status_id', 'deadline')
-    ),
+            ],
+        ],
+        'table_columns' => ['name', 'project', 'status_id', 'deadline']
+    ],
 
-    'my_tasks_due_today' => array(
+    'my_tasks_due_today' => [
         'obj_type' => 'task',
         'name' => 'My Incomplete Tasks (due today)',
         'description' => 'Incomplete tasks assigned to me that are due today',
         'default' => false,
-        'conditions' => array(
-            'user' => array(
+        'conditions' => [
+            'user' => [
                 'blogic' => Where::COMBINED_BY_AND,
                 'field_name' => 'owner_id',
                 'operator' => Where::OPERATOR_EQUAL_TO,
                 'value' => UserEntity::USER_CURRENT,
-            ),
-            'status_id' => array(
+            ],
+            'status_id' => [
                 'blogic' => Where::COMBINED_BY_AND,
                 'field_name' => 'status_id',
                 'operator' => Where::OPERATOR_NOT_EQUAL_TO,
                 'value' => TaskEntity::STATUS_COMPLETED
-            ),
-            'deadline' => array(
+            ],
+            'deadline' => [
                 'blogic' => Where::COMBINED_BY_AND,
                 'field_name' => 'deadline',
                 'operator' => Where::OPERATOR_LESS_THAN_OR_EQUAL_TO,
                 'value' => 'now'
-            ),
-        ),
-        'order_by' => array(
-            'date' => array(
+            ],
+        ],
+        'order_by' => [
+            'date' => [
                 'field_name' => 'ts_entered',
                 'direction' => 'desc',
-            ),
-            'deadline' => array(
+            ],
+            'deadline' => [
                 'field_name' => 'deadline',
                 'direction' => 'asc'
-            ),
-        ),
-        'table_columns' => array('name', 'project', 'status_id', 'deadline')
-    ),
+            ],
+        ],
+        'table_columns' => ['name', 'project', 'status_id', 'deadline']
+    ],
 
-    'all_my_tasks' => array(
+    'all_my_tasks' => [
         'obj_type' => 'task',
         'name' => 'All My Tasks',
         'description' => 'All tasks assigned to me',
         'default' => false,
-        'conditions' => array(
-            'user' => array(
+        'conditions' => [
+            'user' => [
                 'blogic' => Where::COMBINED_BY_AND,
                 'field_name' => 'owner_id',
                 'operator' => Where::OPERATOR_EQUAL_TO,
                 'value' => UserEntity::USER_CURRENT,
-            ),
-        ),
-        'order_by' => array(
-            'date' => array(
+            ],
+        ],
+        'order_by' => [
+            'date' => [
                 'field_name' => 'ts_entered',
                 'direction' => 'desc',
-            ),
-            'deadline' => array(
+            ],
+            'deadline' => [
                 'field_name' => 'deadline',
                 'direction' => 'asc'
-            ),
-        ),
-        'table_columns' => array('name', 'project', 'status_id', 'deadline')
-    ),
+            ],
+        ],
+        'table_columns' => ['name', 'project', 'status_id', 'deadline']
+    ],
 
-    'tasks_i_have_assigned' => array(
+    'tasks_i_have_assigned' => [
         'obj_type' => 'task',
         'name' => 'Tasks I Have Assigned',
         'description' => 'Tasks that were created by me but assigned to someone else',
         'default' => false,
         'group_first_order_by' => true,
-        'conditions' => array(
-            'creator' => array(
+        'conditions' => [
+            'creator' => [
                 'blogic' => Where::COMBINED_BY_AND,
                 'field_name' => 'creator_id',
                 'operator' => Where::OPERATOR_EQUAL_TO,
                 'value' => UserEntity::USER_CURRENT,
-            ),
-            'user' => array(
+            ],
+            'user' => [
                 'blogic' => Where::COMBINED_BY_AND,
                 'field_name' => 'owner_id',
                 'operator' => Where::OPERATOR_NOT_EQUAL_TO,
                 'value' => UserEntity::USER_CURRENT,
-            ),
-            'status_id' => array(
+            ],
+            'status_id' => [
                 'blogic' => Where::COMBINED_BY_AND,
                 'field_name' => 'status_id',
                 'operator' => Where::OPERATOR_NOT_EQUAL_TO,
                 'value' => TaskEntity::STATUS_COMPLETED
-            ),
-        ),
-        'order_by' => array(
-            'date' => array(
+            ],
+        ],
+        'order_by' => [
+            'date' => [
                 'field_name' => 'ts_entered',
                 'direction' => 'desc',
-            ),
-            'deadline' => array(
+            ],
+            'deadline' => [
                 'field_name' => 'deadline',
                 'direction' => 'asc'
-            ),
-        ),
-        'table_columns' => array('name', 'project', 'status_id', 'deadline', 'owner_id')
-    ),
+            ],
+        ],
+        'table_columns' => ['name', 'project', 'status_id', 'deadline', 'owner_id']
+    ],
 
-    'all_incomplete_tasks' => array(
+    'all_incomplete_tasks' => [
         'obj_type' => 'task',
         'name' => 'All Incomplete Tasks',
         'description' => 'All Tasks that have not yet been completed',
         'default' => false,
-        'conditions' => array(
-            'status_id' => array(
+        'conditions' => [
+            'status_id' => [
                 'blogic' => Where::COMBINED_BY_AND,
                 'field_name' => 'status_id',
                 'operator' => Where::OPERATOR_NOT_EQUAL_TO,
                 'value' => TaskEntity::STATUS_COMPLETED
-            ),
-        ),
-        'order_by' => array(
-            'status_id' => array(
+            ],
+        ],
+        'order_by' => [
+            'status_id' => [
                 'field_name' => 'status_id',
                 'direction' => 'desc',
-            ),
-            'date' => array(
+            ],
+            'date' => [
                 'field_name' => 'date_completed',
                 'direction' => 'desc',
-            ),
-            'deadline' => array(
+            ],
+            'deadline' => [
                 'field_name' => 'deadline',
                 'direction' => 'asc'
-            ),
-        ),
-        'table_columns' => array('name', 'project', 'status_id', 'deadline', 'owner_id')
-    ),
+            ],
+        ],
+        'table_columns' => ['name', 'project', 'status_id', 'deadline', 'owner_id']
+    ],
 
-    'all_tasks' => array(
+    'all_tasks' => [
         'obj_type' => 'task',
         'name' => 'All Tasks',
         'description' => 'All Tasks',
         'default' => false,
-        'order_by' => array(
-            'date' => array(
+        'order_by' => [
+            'date' => [
                 'field_name' => 'ts_entered',
                 'direction' => 'desc',
-            ),
-            'deadline' => array(
+            ],
+            'deadline' => [
                 'field_name' => 'deadline',
                 'direction' => 'asc'
-            ),
-        ),
-        'table_columns' => array('name', 'project', 'status_id', 'deadline', 'owner_id')
-    ),
-);
+            ],
+        ],
+        'table_columns' => ['name', 'project', 'status_id', 'deadline', 'owner_id']
+    ],
+];
