68d7b73678ef7632e4c4fa2819853352ad48626e
Sky Stebnicki <sky@stebnicki.net>
2021-03-12 13:38:04 -0500

    Fixed query to default to sky if the note is empty

--- bin/scripts/update/once/005/001/003.php
+++ bin/scripts/update/once/005/001/003.php
@@ -16,13 +16,16 @@ $entityFactory = $serviceManager->get(EntityFactoryFactory::class);
 $entityLoader = $serviceManager->get(EntityLoaderFactory::class);
 
 
-$result = $db->query("select field_data, entity_id from entity where entity_definition_id='00000000-0000-0000-0000-000000000028' and field_data->>'owner_id' IS NULL and field_data->>'user_id' IS NOT NULL");
+$result = $db->query("select field_data, entity_id from entity where entity_definition_id='00000000-0000-0000-0000-000000000028' and field_data->>'owner_id' IS NULL");
 for ($i = 0; $i < $result->rowCount(); $i++) {
     $row = $result->fetch();
     $data = json_decode($row['field_data'], true);
 
-    // Copy user_id to owner_id so the queries work
-    $data['owner_id'] = $data['user_id'];
+    // Copy user_id to owner_id, or move to sky UUID if user_id is empty
+    $data['owner_id'] = '00000000-0000-0000-0000-000006b40500';
+    if (isset($data['user_id']) && !empty($data['user_id'])) {
+        $data['owner_id'] = $data['user_id'];
+    }
 
     $db->query(
         "UPDATE entity SET field_data=:field_data WHERE entity_id=:entity_id",
