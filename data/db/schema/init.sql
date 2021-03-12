6597e1fe7accfb98e339730296f81bf6e869a5b3
Sky Stebnicki <sky@stebnicki.net>
2021-03-12 11:56:25 -0500

    Removed line that was causing failure

--- data/db/schema/init.sql
+++ data/db/schema/init.sql
@@ -7,18 +7,11 @@ SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
-SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
-
---
--- Name: SCHEMA public; Type: COMMENT; Schema: -
---
 SET default_tablespace = '';
-SET default_table_access_method = heap;
-
+SET default_with_oids = false;
 
 --
 -- TABLE: account
