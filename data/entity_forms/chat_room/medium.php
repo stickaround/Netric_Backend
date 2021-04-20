87734649439df3fe1d5a441af982809e47f0fdc3
sky <sky.stebnicki@aereus.com>
2021-04-20 12:36:32 -0400

    Updated the form for chat_root to include members plugin

--- data/entity_forms/chat_room/medium.php
+++ data/entity_forms/chat_room/medium.php
@@ -1,2 +1,21 @@
-<field name='subject' hidelabel="t" class='headline'></field>
-<chat_room_messages></chat_room_messages>
+<row>
+    <column>
+        <row>
+            <column>
+                <row>
+                    <column>
+                        <field name='subject' hidelabel="t" class='headline'></field>
+                    </column>
+                </row>
+            </column>
+        </row>
+        <row>
+            <column>
+                <chat_room_messages></chat_room_messages>
+            </column>
+            <column type="sidebar">
+                <plugin name='Members' field='members'></plugin>
+            </column>
+        </row>
+    </column>
+</row>
