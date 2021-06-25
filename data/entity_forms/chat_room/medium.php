0cd277397862f2490d0daab285dea7550941a6ef
Sky Stebnicki <sky@stebnicki.net>
2021-06-25 18:12:30 -0400

    Changed form for chat_room to be fully handled by the UI

--- data/entity_forms/chat_room/medium.php
+++ data/entity_forms/chat_room/medium.php
@@ -1,21 +1,5 @@
 <row>
     <column>
-        <row>
-            <column>
-                <row>
-                    <column>
-                        <field name='subject' hidelabel="t" class='headline'></field>
-                    </column>
-                </row>
-            </column>
-        </row>
-        <row>
-            <column>
-                <chat_room_messages></chat_room_messages>
-            </column>
-            <column type="sidebar">
-                <members field='members' />
-            </column>
-        </row>
+        <chat_room />
     </column>
 </row>
--- data/entity_forms/chat_room/small.php
+++ data/entity_forms/chat_room/small.php
@@ -1,3 +1,5 @@
-<field name='subject' hidelabel="t" class='headline'></field>
-<chat_room_messages></chat_room_messages>
-<members field='members' />
+<row>
+    <column>
+        <chat_room />
+    </column>
+</row>
