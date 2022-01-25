520f20f2960f0a24614bbd6df6f44f6c66fe00d6
marl.tumulak <marl.tumulak@gmail.com>
2022-01-25 17:57:10 +0800

    Summary: Added the entity forms for contacts that will be used in account billing component.
    Test Plan: Login to netric client, go to settings module and open accounts billing page
    
    Reviewers: sky
    
    Subscribers: joemariel
    
    Differential Revision: https://src.aereus.com/D1402

---
+++ data/entity_forms/contact/billing_small.php
@@ -0,0 +1,7 @@
+<field name='first_name'></field>
+<field name='last_name'></field>
+<field name='billing_street'></field>
+<field name='billing_street2'></field>    
+<field name='billing_city'></field> 
+<field name='billing_state'></field>
+<field name='billing_zip'></field>
