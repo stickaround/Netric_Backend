c6da48fb8174fe9952b1c1765c842a72f74e5806
Sky Stebnicki <sky@stebnicki.net>
2022-02-02 13:34:41 -0500

    Created a link for the post

--- data/entity_definitions/status_update.php
+++ data/entity_definitions/status_update.php
@@ -37,20 +37,8 @@ return [
             'subtype' => '',
             'readonly' => true
         ],
-        'link_title' => [
-            'title' => 'Link Title',
-            'type' => Field::TYPE_TEXT,
-            'subtype' => '',
-            'readonly' => false,
-        ],
-        'link_image_url' => [
-            'title' => 'Link Image',
-            'type' => Field::TYPE_TEXT,
-            'subtype' => '',
-            'readonly' => false,
-        ],
-        'link_description' => [
-            'title' => 'Link Description',
+        'link' => [
+            'title' => 'Link',
             'type' => Field::TYPE_TEXT,
             'subtype' => '',
             'readonly' => false,
