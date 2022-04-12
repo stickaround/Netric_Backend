08f04c5a1954f39c0fb4a6ab2069b76763cf8286
Sky Stebnicki <sky@stebnicki.net>
2022-04-12 18:31:40 -0400

    Fixed bad query

--- data/db/schema/v5-rename-ic-document.sql
+++ data/db/schema/v5-rename-ic-document.sql
@@ -1,2 +1,2 @@
 -- We are renaming "infocenter_document" to "document"
-UPDATE entity_definition SET name="document" WHERE name="infocenter_document";
+UPDATE entity_definition SET name='document' WHERE name='infocenter_document';
