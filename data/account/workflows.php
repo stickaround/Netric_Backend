387dd691fbd8da1c2de5628d7cd35921ef335513
Sky Stebnicki <sky@stebnicki.net>
2022-04-22 14:25:58 -0400

    Adding workflow for tasks

--- data/account/workflows.php
+++ data/account/workflows.php
@@ -3,6 +3,7 @@
 namespace data\account;
 
 use Netric\Entity\ObjType\TaskEntity;
+use Netric\Entity\ObjType\TicketEntity;
 use Netric\EntityQuery\Where;
 use Netric\EntityDefinition\ObjectTypes;
 use Netric\Workflow\WorkflowScheudleTimes;
@@ -70,6 +71,62 @@ return [
                                     'update_value' => true
                                 ],
                             ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+    [
+        "name" => "Close Completed Ticket",
+        "uname" => "ticket-close-on-complete",
+        "notes" => "Closes a ticket if it gets marked as completed.",
+        "object_type" => ObjectTypes::TICKET,
+        "f_active" => true,
+        "f_on_create" => true,
+        "f_on_update" => true,
+        "f_on_delete" => false,
+        "f_on_daily" => false,
+        "f_singleton" => false,
+        "f_system" => true,
+        "child_actions" => [
+            [
+                "name" => "Check for Closed Conditions",
+                "uname" => "ticket-close-on-complete-condition",
+                "type_name" => "check_condition",
+                "f_system" => true,
+                "data" => [
+                    // Launch if status=solved or status=unsolvable
+                    'conditions' => [
+                        [
+                            'blogic' => Where::COMBINED_BY_AND,
+                            'field_name' => 'is_closed',
+                            'operator' => Where::OPERATOR_NOT_EQUAL_TO,
+                            'value' => true
+                        ],
+                        [
+                            'blogic' => Where::COMBINED_BY_AND,
+                            'field_name' => 'status_id',
+                            'operator' => Where::OPERATOR_EQUAL_TO,
+                            'value' => TicketEntity::STATUS_SOLVED,
+                        ],
+                        [
+                            'blogic' => Where::COMBINED_BY_OR,
+                            'field_name' => 'status_id',
+                            'operator' => Where::OPERATOR_EQUAL_TO,
+                            'value' => TicketEntity::STATUS_UNSOLVABLE,
+                        ]
+                    ]
+                ],
+                "child_actions" => [
+                    [
+                        "name" => "Close the Ticket",
+                        "uname" => "ticket-close-on-complete-close",
+                        "type_name" => "update_field",
+                        "f_system" => true,
+                        "data" => [
+                            'update_field' => "is_closed",
+                            'update_value' => true
                         ],
                     ],
                 ],
