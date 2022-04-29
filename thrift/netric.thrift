ec63434e2083e0d89a17f69e20bdbdefd3a3bc00
Sky Stebnicki <sky@stebnicki.net>
2022-04-29 06:02:38 -0400

    Merged thrift files

--- thrift/netric.thrift
+++ thrift/netric.thrift
@@ -1,8 +1,6 @@
 namespace php NetricApi
 namespace js NetricApi
 
-include "entityquery.thrift"
-
 /**
  * Invalid arguments sent
  */
@@ -39,6 +37,12 @@ service Test
     string ping();
 }
 
+
+/**
+ * SERVICE: Entity
+ * ==========================================================
+ */
+
 /**
  * Entity service
  */
@@ -56,6 +60,12 @@ service Entity
         throws (1:ErrorException error, 2:InvalidArgument badRequest);
 }
 
+
+/**
+ * SERVICE: Chat
+ * ==========================================================
+ */
+
 /**
  * Chat service used for chat-specific operations
  */
@@ -66,6 +76,11 @@ service Chat
      */
     void notifyAbsentOfNewMessage(1:string messageId, 3:string accountId);
 }
+
+/**
+ * SERVICE: Worker
+ * ==========================================================
+ */
 
 /**
  * The worker service handles processing queued jobs
@@ -79,3 +94,39 @@ service Worker
         throws (1:ErrorException error, 2:InvalidArgument badRequest) ;
 }
 
+/**
+ * SERVICE: EntityQuery
+ * ==========================================================
+ */
+
+/**
+ * Define query type data
+ */
+struct EntityQueryData {
+    1: string obj_type;
+    2: int32 offset;
+    3: int32 limit;
+}
+
+/**
+ * Define what an entity query result will look like
+ */
+struct EntityQueryResultData {
+    1: EntityQueryData query_ran;
+    2: string account;
+    3: int32 num;
+    4: int32 total_num;
+    5: map<string,string> entities
+}
+
+/**
+ * Entity query service
+ */
+service EntityQuery
+{
+    /**
+     * Update the user last active
+     */
+    string execute(1:string userId, 2:string accountId, 3:string jsonQuery) 
+        throws (1:ErrorException error, 2:InvalidArgument badRequest);
+}
